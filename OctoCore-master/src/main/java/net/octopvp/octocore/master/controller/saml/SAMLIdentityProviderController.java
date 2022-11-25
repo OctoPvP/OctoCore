736bf5a7e06486897605704d0992f5101d07876f
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-25 11:52:58 -0500

    idk

--- OctoCore-master/src/main/java/net/octopvp/octocore/master/controller/saml/SAMLIdentityProviderController.java
+++ OctoCore-master/src/main/java/net/octopvp/octocore/master/controller/saml/SAMLIdentityProviderController.java
@@ -1,9 +1,12 @@
 package net.octopvp.octocore.master.controller.saml;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/saml")
 public class SAMLIdentityProviderController {
 
