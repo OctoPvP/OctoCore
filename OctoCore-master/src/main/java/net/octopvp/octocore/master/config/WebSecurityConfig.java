64b237eb02cf9f0a462e7622f11ace6951ace18a
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-11-25 12:52:08 -0500

    Revert "idk"
    This reverts commit 736bf5a7e06486897605704d0992f5101d07876f.

--- OctoCore-master/src/main/java/net/octopvp/octocore/master/config/WebSecurityConfig.java
+++ OctoCore-master/src/main/java/net/octopvp/octocore/master/config/WebSecurityConfig.java
@@ -1,72 +1,29 @@
 package net.octopvp.octocore.master.config;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
-import lombok.extern.slf4j.Slf4j;
 import net.octopvp.octocore.master.component.AuthEntryPointJwt;
-import net.octopvp.octocore.master.saml.SAMLConfig;
-import net.octopvp.octocore.master.saml.SamlAppProperties;
 import net.octopvp.octocore.master.services.UserDetailsServiceImpl;
 import net.octopvp.octocore.master.views.pages.impl.login.LoginView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
-import org.springframework.security.saml.provider.identity.config.SamlIdentityProviderSecurityConfiguration;
 
 import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
-
-import static org.springframework.security.saml.provider.identity.config.SamlIdentityProviderSecurityDsl.identityProvider;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 @SuppressWarnings({"unused", "deprecation"})
 public class WebSecurityConfig extends VaadinWebSecurityConfigurerAdapter {
-
-
-    @Configuration
-    @Order(1)
-    @Slf4j
-    public static class SamlSecurity extends SamlIdentityProviderSecurityConfiguration {
-
-        private final SamlAppProperties appProperties;
-        private final SAMLConfig samlConfig;
-        private UserDetailsServiceImpl userDetailsService;
-
-        public SamlSecurity(SAMLConfig samlConfig, @Qualifier("samlAppProperties") SamlAppProperties appProperties, UserDetailsServiceImpl userDetailsService) throws IOException {
-            super("/saml/idp/", samlConfig);
-            this.appProperties = appProperties;
-            this.samlConfig = samlConfig;
-            this.userDetailsService = userDetailsService;
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            super.configure(http);
-            http.
-                    userDetailsService(userDetailsService)
-                            .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/login"));
-        //.formLogin();
-            //http.authorizeRequests()
-            //                .antMatchers("/saml/**").permitAll();
-            http.
-                    apply(identityProvider())
-                    .configure(appProperties);
-        }
-    }
     public static final String LOGOUT_URL = "/";
 
     @Autowired
@@ -113,7 +70,7 @@ public class WebSecurityConfig extends VaadinWebSecurityConfigurerAdapter {
                 });
          */
         http.authorizeRequests()
-                .antMatchers("/img/**", "/dist/**", "/js/**", "/css/**", "/saml/**").permitAll();
+                .antMatchers("/img/**", "/dist/**", "/js/**", "/css/**").permitAll();
 
         super.configure(http);
 
--- OctoCore-master/src/main/java/net/octopvp/octocore/master/controller/saml/SAMLIdentityProviderController.java
+++ OctoCore-master/src/main/java/net/octopvp/octocore/master/controller/saml/SAMLIdentityProviderController.java
@@ -1,12 +1,9 @@
 package net.octopvp.octocore.master.controller.saml;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/saml")
 public class SAMLIdentityProviderController {
 
