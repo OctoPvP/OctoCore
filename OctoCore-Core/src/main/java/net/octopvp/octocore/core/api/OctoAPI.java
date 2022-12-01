2b65439ab34d10ef1fa59f3190c79595479dc668
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-12-01 12:36:47 -0500

    oops missed some

--- OctoCore-Core/src/main/java/net/octopvp/octocore/core/api/OctoAPI.java
+++
@@ -1,9 +0,0 @@
-package net.octopvp.octocore.core.api;
-
-import net.octopvp.octocore.core.objects.PlayerData;
-
-import java.util.UUID;
-
-public interface OctoAPI {
-    PlayerData getPlayerData(UUID uuid);
-}
