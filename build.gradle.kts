ee0d7dda9115faab84b0088458845b8382467452
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-01-01 00:28:34 -0500

    clean up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,2 @@
-plugins {
-    id("net.octopvp.java-conventions")
-}
-
-dependencies {
-    /*
-    implementation(project(":OctoCore-Core", "shadow"))
-    implementation(project(":OctoCore-1.8"))
-    implementation(project(":OctoCore-waterfall", "shadow"))
-    implementation(project(":OctoCore-common", "shadow"))
-     */
-}
-
 description = "OctoCore Parent"
 
