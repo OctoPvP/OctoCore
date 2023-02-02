845ff7d4c43ec3a8b01f2e9e55115078ba80112d
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-02-02 13:02:56 -0500

    update redis channel

--- OctoCore-common/src/main/java/net/octopvp/octocore/common/redis/RedisManager.java
+++ OctoCore-common/src/main/java/net/octopvp/octocore/common/redis/RedisManager.java
@@ -13,7 +13,7 @@ import redis.clients.jedis.JedisPool;
 @Getter
 public class RedisManager {
 
-    private static final String CHANNEL = "aec", DEV_BRIDGE_CHANNEL = "aec:dev_bridge";
+    private static final String CHANNEL = "octo", DEV_BRIDGE_CHANNEL = "octo:dev_bridge";
     private static final JsonParser jsonParser = new JsonParser();
     @Getter
     @Setter
