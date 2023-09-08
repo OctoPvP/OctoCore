ba787f1cc3c9dc9343ed84692618d88aa354a6ed
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-09-08 12:44:09 -0400

    debug node tree

--- OctoCore-Core/src/main/java/net/octopvp/octocore/core/command/impl/rank/PermissionInfoCommand.java
+++ OctoCore-Core/src/main/java/net/octopvp/octocore/core/command/impl/rank/PermissionInfoCommand.java
@@ -1,13 +1,11 @@
 package net.octopvp.octocore.core.command.impl.rank;
 
-import net.octopvp.commander.annotation.Command;
-import net.octopvp.commander.annotation.Optional;
-import net.octopvp.commander.annotation.Required;
-import net.octopvp.commander.annotation.Sender;
+import net.octopvp.commander.annotation.*;
 import net.octopvp.commander.bukkit.annotation.DefaultSelf;
 import net.octopvp.commander.bukkit.annotation.PlayerOnly;
 import net.octopvp.octocore.common.util.CC;
 import net.octopvp.octocore.common.util.perms.PermissionCheckResult;
+import net.octopvp.octocore.common.util.perms.PermissionManager;
 import net.octopvp.octocore.core.command.CommandResult;
 import net.octopvp.octocore.core.objects.PlayerData;
 import org.bukkit.entity.Player;
@@ -15,7 +13,16 @@ import org.bukkit.entity.Player;
 public class PermissionInfoCommand {
     @Command(name = "haspermission", aliases = {"permissioninfo", "perminfo"}, description = "Shows information about a player's permission")
     @PlayerOnly
-    public CommandResult execute(@Sender Player sender, @Required String permission, @Optional @DefaultSelf PlayerData target) {
+    public CommandResult execute(@Sender Player sender, @Required String permission, @Optional @DefaultSelf PlayerData target, @Switch("p") boolean printNodes) {
+        if (printNodes) {
+            System.out.println("----------------------------");
+            System.out.println("Final Nodes: ");
+            PermissionManager.getInstance().printNodeMap(target.getFinalNodes());
+            System.out.println("----------------------------");
+            System.out.println("Rank Nodes: ");
+            PermissionManager.getInstance().printNodeMap(target.getHighestRank().getFinalNodes());
+            System.out.println("----------------------------");
+        }
         PermissionCheckResult result = target.getPermissionResult(permission);
         sender.sendMessage(CC.SEPARATOR);
         sender.sendMessage(CC.PRIMARY + "Permission Info For: " + CC.SECONDARY + target.getDisplayName());
