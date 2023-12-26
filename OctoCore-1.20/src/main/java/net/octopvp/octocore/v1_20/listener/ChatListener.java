5efdbfa6889c669f1b5dd1ae16719f234bf4cad5
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-12-26 11:40:37 -0500

    fix chat renderer (x2)

--- OctoCore-1.20/src/main/java/net/octopvp/octocore/v1_20/listener/ChatListener.java
+++ OctoCore-1.20/src/main/java/net/octopvp/octocore/v1_20/listener/ChatListener.java
@@ -16,13 +16,13 @@ import org.jetbrains.annotations.NotNull;
 public class ChatListener implements Listener {
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onChat(AsyncChatEvent e) { // the cool new listener
-        Component component = ChatManager.formatChatComponent(e.getPlayer(),
-                e.message(),
-                e.getPlayer().hasPermission(Permissions.USE_COLOR_CHAT)
-        );
+
         e.renderer(new ChatRenderer() {
             @Override
             public @NotNull Component render(@NotNull Player source, @NotNull Component sourceDisplayName, @NotNull Component message, @NotNull Audience viewer) {
+                Component component = ChatManager.formatChatComponent(source, message,
+                        e.getPlayer().hasPermission(Permissions.USE_COLOR_CHAT)
+                ); // TODO: chat hook for TeamsPlus
                 return component == null ? Component.empty() : component;
             }
         });
