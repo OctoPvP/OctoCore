f8e5e5b896290a7d44fd16c05590d8d2be126f74
Pylons <ngedwards998@gmail.com>
2024-10-03 16:49:20 -0700

    update OctoCore to 1.21

--- OctoCore-1.20/build.gradle.kts
+++ OctoCore-1.20/build.gradle.kts
@@ -21,14 +21,14 @@ repositories {
 dependencies {
     implementation(project(":OctoCore-Core"))
     implementation(project(":OctoCore-common"))
-    paperweightDevBundle("net.octopvp.octospigot", "1.20.4-R0.1-SNAPSHOT")
+    paperweightDevBundle("net.octopvp.octospigot", "1.21.1-R0.1-SNAPSHOT")
 }
 tasks {
     assemble {
         dependsOn(reobfJar)
     }
     shadowJar {
-        archiveFileName.set("OctoCore-1.20.jar")
+        archiveFileName.set("OctoCore-1.21.jar")
     }
     withType<ProcessResources> {
         filesMatching("plugin.yml") {
@@ -40,7 +40,7 @@ tasks {
 
         // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
         // See https://openjdk.java.net/jeps/247 for more information.
-        options.release.set(17)
+        options.release.set(21)
     }
     javadoc {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
@@ -56,5 +56,5 @@ tasks {
 tasks.getByName("build").dependsOn("shadowJar")
 java {
     // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
