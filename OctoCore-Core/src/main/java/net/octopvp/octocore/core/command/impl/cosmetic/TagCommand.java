717e93e6ef5107a459d6d69d7ccd3aeb9250d626
Evan <50347938+Badbird5907@users.noreply.github.com>
2024-12-02 10:50:30 -0500

    fix: tag command

--- OctoCore-Core/src/main/java/net/octopvp/octocore/core/command/impl/cosmetic/TagCommand.java
+++ OctoCore-Core/src/main/java/net/octopvp/octocore/core/command/impl/cosmetic/TagCommand.java
@@ -3,15 +3,13 @@ package net.octopvp.octocore.core.command.impl.cosmetic;
 import net.octopvp.commander.annotation.Command;
 import net.octopvp.commander.annotation.Sender;
 import net.octopvp.commander.bukkit.annotation.PlayerOnly;
-import net.octopvp.octocore.core.command.CommandResult;
 import net.octopvp.octocore.core.menus.tag.MainTagMenu;
 import org.bukkit.entity.Player;
 
 public class TagCommand {
     @Command(name = "tag", aliases = "tags")
     @PlayerOnly
-    public CommandResult execute(@Sender Player sender) {
+    public void execute(@Sender Player sender) {
         new MainTagMenu().open(sender);
-        return CommandResult.SUCCESS;
     }
 }
